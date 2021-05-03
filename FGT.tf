5959f1ddcbc32a88c65f80c52c0f0afd9fc289e1
waleedrady <82145296+waleedrady@users.noreply.github.com>
2021-05-03 09:15:37 -0400

    Create FGT.tf

---
+++ FGT.tf
@@ -0,0 +1,28 @@
+module "FGT-SDWAN-Module" {
+  source          = "WEEMR/FGT-SDWAN-Module/aws"
+  version         = "0.0.1"
+  access_key      = " < Access Key > "                         #  IAM Account Access Key
+  secret_key      = " < Secret Key > "                         #  IAM Account Secret Key
+  Resources_Owner = " < Name > "                               #  Your 1st Initial, Last Name to tag resources
+  keyname         = " < AWS Keypair > "                        #  Your AWS Keypair to create resources
+}
+
+
+
+#  ----------------  Outputs  -----------------  # 
+
+output "FGT_WAN1_IP" {
+  value = module.FGT-SDWAN-Module.FGT_WAN1_IP
+}
+
+output "FGT_WAN2_IP" {
+  value = module.FGT-SDWAN-Module.FGT_WAN2_IP
+}
+
+output "Username" {
+  value = module.FGT-SDWAN-Module.Username
+}
+
+output "FGT_Password" {
+  value = module.FGT-SDWAN-Module.FGT_Password
+}
